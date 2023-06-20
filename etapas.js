90d46a8801943126040991912091d0692b26812f
Cristiano Filho ✅ <54041918+CristianoFIlho@users.noreply.github.com>
2023-06-19 21:28:05 -0300

    Update etapas.js

--- etapas.js
+++ etapas.js
@@ -1,55 +1,38 @@
-	let etapas = [
-	{
-		titulo: 'Cantor ou cantora',
-		numeros: 2,
-		candidatos: [
-
-	{
-		numero: '01',
-		nome: 'Ivete Sangalo',
-		foto: [{url: 'IveteSangalo.png', legenda: 'Cantor(a)'}]
-		
-		   },
-
-		   {
-		numero: '02',
-		nome: 'Gilberto Gil',
-		foto: [{url: 'GilbertoGil.png', legenda: 'Cantor(a)'}]
-		
-		   },
-
-	 {
-		numero: '03',
-		nome: 'Léo Santana',
-		foto: [{url: 'LeoSantana.png', legenda: 'Cantor(a)'}]
-		
-		   },
-
-	{
-		numero: '04',
-		nome: 'Saulo Fernandes',
-		foto: [{url: 'Saulo.png', legenda: 'Cantor(a)'}]
-		
-		   },
-
-	{
-		numero: '05',
-		nome: 'Caetano Veloso',
-		foto: [{url: 'Caetano.png', legenda: 'Cantor(a)'}]
-		
-		   },
-
-
-	 {
-		numero: '06',
-		nome: 'Gal Costa',
-		foto: [{url: 'GalCosta.png', legenda: 'Cantor(a)'}]
-		
-		  },
-
-	  ]
-
- }
-
- ]
- 
+let etapas = [
+  {
+    titulo: 'Cantor ou cantora',
+    numeros: 2,
+    candidatos: [
+      {
+        numero: '01',
+        nome: 'Michael Jackson',
+        foto: [{ url: 'IveteSangalo.png', legenda: 'Cantor(a)' }]
+      },
+      {
+        numero: '02',
+        nome: 'Elvis Presley',
+        foto: [{ url: 'GilbertoGil.png', legenda: 'Cantor(a)' }]
+      },
+      {
+        numero: '03',
+        nome: 'Freddie Mercury',
+        foto: [{ url: 'LeoSantana.png', legenda: 'Cantor(a)' }]
+      },
+      {
+        numero: '04',
+        nome: 'Whitney Houston',
+        foto: [{ url: 'Saulo.png', legenda: 'Cantor(a)' }]
+      },
+      {
+        numero: '05',
+        nome: 'Aretha Franklin',
+        foto: [{ url: 'Caetano.png', legenda: 'Cantor(a)' }]
+      },
+      {
+        numero: '06',
+        nome: 'Bob Marley',
+        foto: [{ url: 'GalCosta.png', legenda: 'Cantor(a)' }]
+      }
+    ]
+  }
+];
